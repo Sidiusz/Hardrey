65258ee8cc489553cd421b3a1f7d017e1bf6f332
Sidiusz <sidiusz1337@gmail.com>
2022-12-30 17:11:36 +0400

    lheu

--- Assets/Source/Scripts/Player/ButtonHover.cs
+++ Assets/Source/Scripts/Player/ButtonHover.cs
@@ -1,26 +1,43 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.EventSystems;
 
-public class ButtonHover : MonoBehaviour
+public class ButtonHover : MonoBehaviour//, IPointerEnterHandler, IPointerExitHandler
 {
-    public TextMeshProUGUI text;
-    public Color normalColor = Color.white;
+    /*public TextMeshProUGUI text;
+    public Color normalColor1 = Color.white;
+    public Color normalColor2 = Color.white;
     public Color hoverColor1 = new Color32(251, 170, 128, 255);
     public Color hoverColor2 = new Color32(249, 191, 115, 255);
 
-private void Start()
+private TMP_ColorGradient normalGradient;
+    private TMP_ColorGradient hoverGradient;
+
+    public void OnPointerEnter(PointerEventData eventData)
     {
-        text.colorGradient = new VertexGradient(normalColor, normalColor, normalColor, normalColor);
+        OnHover();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        OnExit();
+    }
+
+    private void Start()
+    {
+        normalGradient = new TMP_ColorGradient(normalColor1, normalColor2);
+        hoverGradient = new TMP_ColorGradient(hoverColor1, hoverColor2);
+        text.color = normalGradient.topLeft;
     }
 
     public void OnHover()
     {
-        text.colorGradient = new VertexGradient(hoverColor1, hoverColor1, hoverColor2, hoverColor2);
+        text.color = hoverGradient.topLeft;
     }
 
     public void OnExit()
     {
-        text.colorGradient = new VertexGradient(normalColor, normalColor, normalColor, normalColor);
-    }
+        text.color = normalGradient.topLeft;
+    }*/
 }
