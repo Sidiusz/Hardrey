e172e77bee545d05f0e0ce2f91c1260aa26da03d
Sidiusz <sidiusz1337@gmail.com>
2022-12-30 09:21:35 +0400

    upd

--- Assets/Source/Scripts/Player/SaveManager.cs
+++ Assets/Source/Scripts/Player/SaveManager.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class SaveManager : MonoBehaviour
 {
     public string saveSceneName;
     public Vector3 savePosition;
     public GameObject deathPanel;
+    public GameObject savePanel;
     public PlayerController playerController;
+    public TMP_Text deathCountText;
+
+    private int deathCount;
 
     public void SaveGame()
     {
-        // Save current scene name and player position
         saveSceneName = SceneManager.GetActiveScene().name;
         savePosition = transform.position;
     }
@@ -21,37 +25,35 @@ public class SaveManager : MonoBehaviour
     {
         if (SceneManager.GetActiveScene().name == saveSceneName)
         {
-            // Teleport player to save position
             transform.position = savePosition;
         }
         else
         {
-            // Load save scene and teleport player to save position
             SceneManager.LoadScene(saveSceneName);
             transform.position = savePosition;
         }
 
-        // Enable player control
         playerController.controlDisabled = false;
-
-        // Hide death panel
         deathPanel.SetActive(false);
+
+        deathCount = PlayerPrefs.GetInt("deathCount", 0);
+        deathCountText.text = deathCount.ToString();
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "DangerousObject")
         {
-            // Disable player control
             playerController.controlDisabled = true;
-
-            // Show death panel
             deathPanel.SetActive(true);
+            deathCount++;
+            deathCountText.text = deathCount.ToString();
+            PlayerPrefs.SetInt("deathCount", deathCount);
         }
         else if (other.tag == "SavePoint")
         {
-            // Save current scene name and player position
             SaveGame();
+            savePanel.SetActive(true);
         }
     }
 
